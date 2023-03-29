749d7a853cc622705e1a519e95ba51de0514af1b
Julie Combeau <combeau.julie@gmail.com>
2023-03-29 11:24:27 +0200

    test reload image

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD</h1>
+          <h1>CITYCOMPASS Book with CD</h1>
         </Container>
       </Header>
       <Container>
