2748cdfdb5a2bab9224bb0f6e59d5ed3d0ad482c
Julie Combeau <combeau.julie@gmail.com>
2023-03-29 12:37:02 +0200

    test staging n°2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>LA SUPER MODIF</h1>
+          <h1>LA SUPER MODIF n°2</h1>
         </Container>
       </Header>
       <Container>
